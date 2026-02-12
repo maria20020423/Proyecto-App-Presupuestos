56a331e6452f90f7e0d5d58da385fa3bb7c9a65a
maria20020423 <Mariayanettev@gmail.com>
2026-02-11 23:30:19 -0600

    updaitng the configuration

---
+++ README.md
@@ -0,0 +1,23 @@
+## Instal:
+```
+
+sudo apt install libwbclient-dev
+
+```
+
+
+### Commandos
+
+```
+#acces command line
+docker-compose exec firebird bash
+
+#access  datqbase isql 
+CONNECT '/var/lib/firebird/data/mirror.fdb' USER 'maria' PASSWORD 'mariita';
+
+## interactive commands:
+SHOW DATABASE;
+
+```
+
+
--- project/src/index.ts
+++ project/src/index.ts
@@ -1,19 +1,19 @@
-import express from 'express';
+import express from 'express'
 import type { Request, Response } from 'express';
-import {} from 'node-firebird-driver-native';
+import {createNativeClient,getDefaultLibraryFilename} from 'node-firebird-driver-native';
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+  const client=createNativeClient(getDefaultLibraryFilename());
     // Connection options
     const options = {
-      host: '127.0.0.1',
-      port: 3050, // Default Firebird port
-      database: '/home/mozilla/firebird-app/data', // Use an absolute path or alias
-      user: 'maria', // Default username
+      username: 'maria', // Default username
       password: 'mariita', // Default password
     };
+
+  client.connect("localhost/3050:/var/lib/firebird/data/mirror.fdb",options)
 
 app.get('/', (req: Request, res: Response) => {
   res.send({ message: 'TypeScript API is humming along!' });
