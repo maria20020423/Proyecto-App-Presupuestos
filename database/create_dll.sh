52249c1262c69f672e5bfba5b15fa0ca561fc438
maria20020423 <Mariayanettev@gmail.com>
2026-02-21 21:44:00 -0600

    agregando script.sh

---
+++ database/create_dll.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+BASE_DIR=$(dirname "$(readlink -f "$0")")
+DIR_ORIGEN="$BASE_DIR/tablas"
+ARCHIVO_DESTINO="$BASE_DIR/DLL/crear_base_datos.sql"
+mkdir -p "$(dirname "$ARCHIVO_DESTINO")"
+
+# 4. Limpiar o crear el archivo de destino
+> "$ARCHIVO_DESTINO"
+
+if ls "$DIR_ORIGEN"/*.sql >/dev/null 2>&1; then
+    cat "$DIR_ORIGEN"/*.sql > "$ARCHIVO_DESTINO"
+    echo "¡Éxito!"
+    echo "Origen: $DIR_ORIGEN"
+    echo "Destino: $ARCHIVO_DESTINO"
+else
+    echo "Error: No se encontraron archivos .sql en '$DIR_ORIGEN'."
+fi
